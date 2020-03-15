e5324c67a6231106399524c291635a0e6bda5eb2
liweifua <liweifua@126.com>
2020-03-15 17:35:26 +0800

    222

--- src/com/testGit.java
+++ src/com/testGit.java
@@ -1,6 +1,9 @@
 package com;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class testGit {
@@ -8,9 +11,17 @@ public class testGit {
 		System.out.println("test-----git");
 		System.out.println("aaaaaa");
 		try {
-			PrintWriter printWriter = new PrintWriter("d:/aa.txt");
-			printWriter.append("aaaabbbbb");
-		} catch (FileNotFoundException e) {
+//			PrintWriter printWriter = new PrintWriter("d:/aa.txt");
+//			FileWriter printWriter = new FileWriter("d:/aa.txt");
+//			printWriter.write("fdkfdksfdksfdslkgglfkdglsdfdlfkalfkdsflkdslfkl");
+//			printWriter.close();
+			File file=new File("d:\\aa.txt");
+			FileWriter writer=new FileWriter(file);
+			writer.write("hellow，how are you.");
+			writer.close();
+			System.out.println("成功");
+		} catch (IOException e) {
+			System.out.println("erro");
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
